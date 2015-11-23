5ae4fd34472ef9c3b1f4a4d2800ef4642bb16d05
adamvlang <adamlang@kth.se>
2015-11-23 19:10:11 +0100

    More experimenting with matlab

--- active_filter.m
+++ active_filter.m
@@ -7,15 +7,16 @@ C = 100e-12;  % Capacitance [F]
 
 %% Test bode for active filter equation
 % Amplification of op amp
-% A = @(w, RI, RF, CF) -(RI/(RI*(1+1i*w*C*RF))); 
+% A = @(w, RI, RF, CF) -(RF/(RI*(1+1i*w*CF*RF))); 
 % Passive filter 
-A = @(w, RF, CF) 1/(1 + 1i*w*RF*CF); 
+A = @(w, RF, CF) 1/(1 + w*RF*CF); 
 % Decibel conversion
 dB = @(in) 20*log10(in);
 
 freq = 1:1e3;
-for i = freq
-    signal_filtered = A(freq(i), 10e6, 16e-9);  
+for i = 1:length(freq)
+%     signal_filtered = A(freq(i), Ri, Rf, C); 
+    signal_filtered = A(freq(i), 300, 1e-6);  
     amp(i) = dB(abs(signal_filtered));
     arg(i) = phase(signal_filtered);
 %     amp(i) = abs(A(freq(i), Ri, Rf, C));
@@ -28,7 +29,7 @@ title('Bode plot of filter')
 xlabel('Rad/s')
 ylabel('Amplification')
 subplot(2,1,2)
-plot(freq, arg,'r')
+loglog(freq, arg,'r')
 xlabel('Rad/s')
 ylabel('Phase')
 grid on
