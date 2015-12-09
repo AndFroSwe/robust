82b098c306d3eb47cbc67ff38f19ba55a8c801ce
andfroswe <gummianka88@gmail.com>
2015-12-09 11:48:22 +0100

    P controller implemented

---
+++ Labb5/calibrationSensor.m
@@ -0,0 +1,7 @@
+clear all; close all; clc;
+
+ampere = [-0.268 0 0.270];
+voltage = [1 1.87 2.59];
+linearSens = polyfit(voltage, ampere, 1)
+
+plot(ampere, voltage)
--- Labb5/main.c
+++ Labb5/main.c
@@ -4,8 +4,15 @@
 #define H 1
 #define L 0
  
+ float amp(float volt){
+     // Linear calculation of ampere 
+     float k = 0.3373;
+     float m = -0.6133;
+     
+     return k*volt + m;
+     }
 
- 
+
 int main() {
     /*
     PHASE on pin    PWM0    p26
@@ -23,6 +30,7 @@ int main() {
     PwmOut PHASE(p26);
     AnalogOut VREF(p18);
     AnalogIn MEAS_IN(p15);      // Returns values between 0-1.0
+    AnalogIn CONTROL(p16);      // For connection to a pot
     
     // Set Slow current decay mode
     BRAKE = H;
@@ -30,28 +38,56 @@ int main() {
     MODE = L;
     VREF = 0.31f;   // 1 V     
         
-    // Change here
-    float pulswi=20;                        //Pulswidth in %    
-    float freq = 20000;                     // Frequency in Hz                 
-    // calculate
-    float pertid = 1/freq;
+    // Initiate
     float measured = 0;
+    //float control = 0;
+    float dty = 0;
+    float dty_old = dty;
+    
+    // Change here
+    float freq = 50000;                     // Frequency in Hz   
+    float span = 90;                        // Evenly distributed span [%]
+    // Calculate
+    float pertid = 1/freq;
+    //float low_lim = (100 - span)/2/100;         
+
+    // Temporary
+    float ref = 0.05;
 
     clear_disp(); 
 
     while(1) {
+        
+        // For use with changable PWM
+        PHASE.period(pertid);
+        PHASE.pulsewidth(pertid*dty);
+        
         // Read ADC
         measured = MEAS_IN.read();
-        // For use with changable PWM
-        //pulswi = measured*100;
-        PHASE.period(pertid);
-        PHASE.pulsewidth(pertid*pulswi/100);
+        //control = CONTROL.read();
+        // Calculate Duty Cycle with limited span
+        //float dty = low_lim + control*span/100;
+        
+        // Regulator
+        float P = 0.1;
+        
+        float e = ref - amp(measured*3.3);
+        
+        // Regulation
+        dty = dty_old - e*P;
+        dty_old = dty;
+        
+        
         // Write display
         move_cursor(1,1);
         dprintf("Frequency: %i Hz", int(freq));
         move_cursor(2,1);
-        dprintf("PWM: %i %%  ", int(pulswi)); 
+        dprintf("PWM: %i %%  ", int(dty*100)); 
         move_cursor(3, 1);
         dprintf("ADC read: %f V", measured*3.3);
+        move_cursor(4, 1);
+        //dprintf("Control read: %f %%", control*100);
+        dprintf("I: %f  A", amp(measured*3.3));
+        
         }
 }
